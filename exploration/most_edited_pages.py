fe19a31fc94bc700e051ee18df2f15ff2be776cf
clausmie <62254592+ClausMie@users.noreply.github.com>
2026-03-07 00:52:57 +0100

    use correct collumns for tables in use

--- exploration/most_edited_pages.py
+++ exploration/most_edited_pages.py
@@ -1,5 +1,4 @@
 import polars as pl
-import schemas.page_views as spv
 import schemas.page_info as spi
 import schemas.custom as sc
 import schemas.edit_types as sety
@@ -14,9 +13,9 @@ df_edits = pl.read_ndjson("../data/edit_types/dewiki.json.gz")
 df_count = df_edits.group_by(pl.col(sety.PAGE_ID)).len(sc.NUM_EDITS)
 df_count_topN = df_count.top_k(k=top_k, by=sc.NUM_EDITS).sort(by=sc.NUM_EDITS, descending=True)
 
-df_page_info = pl.read_ndjson("../data/page_info.json.gz").select([spv.PAGE_ID, spi.PAGE_TITLE])
+df_page_info = pl.read_ndjson("../data/page_info.json.gz").select([spi.PAGE_ID, spi.PAGE_TITLE])
 
-result = df_count_topN.join(df_page_info, on=spi.PAGE_ID, how="left").top_k(k=top_k, by=sc.NUM_EDITS)
+result = df_count_topN.join(df_page_info, on=sety.PAGE_ID, how="left").top_k(k=top_k, by=sc.NUM_EDITS)
 
 print(result)
 print(result.dtypes)
