561507054c580d2434ff9a70c9f9d5e4f9557a8d
clausmie <62254592+ClausMie@users.noreply.github.com>
2026-03-07 00:20:38 +0100

    join most edited pages in germany on page_id with page_info.json

--- exploration/most_edited_pages.py
+++ exploration/most_edited_pages.py
@@ -1,12 +1,21 @@
 import polars as pl
 
+COLUMN_NAME_NUM_EDITS = "num_edits"
+
+# table<wiki_db: string, items: table<num_edits: int, page_id: int, page_title: string>>
+
 # Number of results
 top_k = 10
 
 # TODO: Iterate over the different files to get result for every language
-df = pl.read_ndjson("../data/edit_types/dewiki.json.gz")
+df_edits = pl.read_ndjson("../data/edit_types/dewiki.json.gz")
 
-df_count = df.group_by(pl.col("page_id")).len()
-df_count_topN = df_count.top_k(k=top_k, by="len").sort(by="len", descending=True)
+df_count = df_edits.group_by(pl.col("page_id")).len(COLUMN_NAME_NUM_EDITS)
+df_count_topN = df_count.top_k(k=top_k, by=COLUMN_NAME_NUM_EDITS).sort(by=COLUMN_NAME_NUM_EDITS, descending=True)
 
-print(df_count_topN)
+df_page_info = pl.read_ndjson("../data/page_info.json.gz").select(["page_id", "page_title"])
+
+result = df_count_topN.join(df_page_info, on="page_id", how="left").top_k(k=top_k, by=COLUMN_NAME_NUM_EDITS)
+
+print(result)
+print(result.dtypes)
