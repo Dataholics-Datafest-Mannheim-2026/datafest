276f07434c306e8df5003e8e5130a94586ac17f4
Jonas Schmidt <jon-schmidt@outlook.de>
2026-03-07 09:03:37 +0100

    updates

--- exploration/topics_per_language/eval.nu
+++ exploration/topics_per_language/eval.nu
@@ -1,3 +1,8 @@
-let res = open result.json
+let page_info = open --raw ../../data/data/page_info.json | from json --objects
+def get_title []: int -> string {
+  try { $page_info | where page_id == $in | first | page_title } catch { null }
+}
 
-$res | group-by country
+let grouped = open result.json | group-by wiki_db --to-table | update items {|r| $r.items | select pageviews page_id | sort-by pageviews | last 10 | insert page_title {|e| $e.page_id | get_title }}
+
+$grouped | save -f ./topics_per_language.yaml
--- exploration/topics_per_language/src/main.rs
+++ exploration/topics_per_language/src/main.rs
@@ -4,13 +4,19 @@ use std::{
     ops::AddAssign,
 };
 
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-const FILE_PATH: &str = "./../page_views.json";
+const FILE_PATH: &str = "../../data/data/page_views.json";
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct JsonEntry {
     pageviews: u64,
+    #[serde(flatten)]
+    key: Key,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, Hash)]
+struct Key {
     page_id: u64,
     wiki_db: heapless::String<10>,
 }
@@ -19,23 +25,26 @@ fn main() -> anyhow::Result<()> {
     let file = std::fs::File::open(FILE_PATH).unwrap();
     let reader = BufReader::new(file);
 
-    let mut index: HashMap<(u64, heapless::String<10>), u64, _> = HashMap::new();
+    let mut index: HashMap<Key, u64, _> = HashMap::new();
 
     for l in reader.lines() {
         if let Ok(l) = l {
             let s: JsonEntry = serde_json::from_str(&l)?;
 
-            if let Some(e) = index.get_mut(&(s.page_id, s.wiki_db.clone())) {
+            if let Some(e) = index.get_mut(&s.key) {
                 e.add_assign(s.pageviews);
             } else {
-                let _ = index.insert((s.page_id, s.wiki_db), s.pageviews);
+                let _ = index.insert(s.key, s.pageviews);
             }
         }
     }
 
-    let index: HashMap<_, _> = index
-        .iter()
-        .map(|e| (format!("{}: {}", e.0.0, e.0.1), e.1))
+    let index: Vec<_> = index
+        .into_iter()
+        .map(|e| JsonEntry {
+            pageviews: e.1,
+            key: e.0,
+        })
         .collect();
 
     let res = serde_json::to_string(&index)?;
